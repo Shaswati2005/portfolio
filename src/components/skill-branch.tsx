54c460310aa025622e05cefbbeea951e190c949c
Shaswati Mishra <mishrashaswati2005@gmail.com>
2025-07-09 12:48:30 +0000

    can you use it in the opposite side  and make it a little bit bigger

--- src/components/skill-branch.tsx
+++ src/components/skill-branch.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function SkillBranch() {
     return (
-        <div className="pointer-events-none absolute -top-24 -left-28 w-[400px] h-auto opacity-50 hidden lg:block z-0">
-             <div className="absolute inset-0 bg-primary/30 rounded-full blur-[100px]" />
-             <div className="animate-sway" style={{ transformOrigin: '90% 90%', animationDuration: '25s' }}>
+        <div className="pointer-events-none absolute -top-28 -right-32 w-[500px] h-auto opacity-50 hidden lg:block z-0">
+             <div className="absolute inset-0 bg-primary/30 rounded-full blur-[120px]" />
+             <div className="animate-sway" style={{ transformOrigin: '10% 90%', animationDuration: '25s' }}>
                 <Image
                   src="/branch.png"
                   alt="Cherry blossom branch"
-                  width={400}
-                  height={300}
-                  className="-rotate-[35deg]"
+                  width={500}
+                  height={375}
+                  className="scale-x-[-1] rotate-[35deg]"
                 />
             </div>
         </div>
